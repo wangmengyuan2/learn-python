9dd79f4f5a1afae8bd8cf515a4d5247001222313
wangmengyuan <524312739@qq.com>
2018-11-20 22:13:40 +0800

    beautify output and add time relavent operation

--- 7_dig.py
+++ 7_dig.py
@@ -1,8 +1,15 @@
 import turtle as t
+import time
+
+def drawGap():
+	t.penup()
+	t.fd(5)
 
 def drawline(draw):
+	drawGap()
 	t.pendown() if draw else t.penup()
 	t.fd(40)
+	drawGap()
 	t.right(90)
 def drawDigit(digit):
 	drawline(True) if digit in [2,3,4,5,6,8,9] else drawline(False)
@@ -16,16 +23,29 @@ def drawDigit(digit):
 	t.left(180)
 	t.penup()
 	t.fd(20)
+
 def drawDate(date):
+	t.pencolor("red")
 	for i in date:
-		drawDigit(eval(i))
+		if i == '-':
+			t.write("Y", font=("Arial", 38, "normal"))
+			t.pencolor("green")
+			t.fd(40)
+		elif i == '=':
+			t.write("M", font=("Arial", 38, "normal"))
+			t.fd(40)
+		elif i == '+':
+			t.write("D", font=("Arial", 38, "normal"))
+			t.fd(40)
+		else:
+			drawDigit(eval(i))
 
 def main():
 	t.setup(800, 350, 200, 200)
 	t.penup()
 	t.fd(-300)
 	t.pensize(5)
-	drawDate('12345678')
+	drawDate(time.strftime("%Y-%m=%d+", time.gmtime()))
 	t.hideturtle()
 	t.done()
 main()
